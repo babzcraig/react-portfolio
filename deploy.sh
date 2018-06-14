fbda135bf985cada4986830aef51fe52e981d398
Babs Craig <babscraig@gmail.com>
2018-06-14 04:53:05 +0100

    add deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-# Create the Heroku app; requires free account at https://www.heroku.com/
-heroku create -b https://github.com/heroku/heroku-buildpack-static.git
+# # Create the Heroku app; requires free account at https://www.heroku.com/
+# heroku create -b https://github.com/heroku/heroku-buildpack-static.git
 
-# Set the web root to the build/ directory
-echo '{ "root": "build/" }' > static.json
-# Allow JS bundle to be committed (removes `build` from ignores)
-sed '/build/d' .gitignore > .gitignore.new && mv .gitignore.new .gitignore
+# # Set the web root to the build/ directory
+# echo '{ "root": "build/" }' > static.json
+# # Allow JS bundle to be committed (removes `build` from ignores)
+# sed '/build/d' .gitignore > .gitignore.new && mv .gitignore.new .gitignore
 
-# Uncomment out the lines above if this is the first time running the app
+# # Uncomment out the lines above if this is the first time running the app
 
 npm run build
 git add .
