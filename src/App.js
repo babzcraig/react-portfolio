3708b27a8de17fc2c72060ccdba13f10c91a24bb
Babs Craig <babscraig@gmail.com>
2018-06-14 03:20:33 +0100

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './Navbar';
-import MainContent from './MainContent';
-import Header from './Header';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import routes from "./constants/routes";
 
 class App extends Component {
   render() {
@@ -16,13 +13,7 @@ class App extends Component {
         <div className="App">
           <Header linkTo={"home"} />
           <Route path="/" component={MainContent} />
-          <Navbar
-            navItems={[
-              {text: "Portfolio", link: "/portfolio"},
-              {text: "Skills", link: "/skills"},
-              {text: "About", link: "/about"},
-              {text: "Connect", link: "/connect"}
-            ]} />
+          <Navbar navItems={routes} />
         </div>
       </Router>
     );
