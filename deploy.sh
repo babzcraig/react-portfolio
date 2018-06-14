7456b61a449dfe366c5bc344f68c2c74edd46068
Babs Craig <babscraig@gmail.com>
2018-06-14 03:23:30 +0100

    deploying to heroku:06/14/18:03:23:30 AM

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# Create the Heroku app; requires free account at https://www.heroku.com/
+heroku create -b https://github.com/heroku/heroku-buildpack-static.git
+
+# Set the web root to the build/ directory
+echo '{ "root": "build/" }' > static.json
+# Allow JS bundle to be committed (removes `build` from ignores)
+sed '/build/d' .gitignore > .gitignore.new && mv .gitignore.new .gitignore
+
+# Uncomment out the lines above if this is the first time running the app
+
+npm run build
+git add .
+now=$(date +"%D:%r")
+git commit -m 'deploying to heroku:'"$now"''
+git push heroku master
+heroku open
