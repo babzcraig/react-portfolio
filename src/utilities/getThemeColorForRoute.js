a3fe9161f0bad6a64d112d291993e03898a3032a
Babs Craig <babscraig@gmail.com>
2018-06-15 00:22:56 +0100

    add helper to get theme color for route

---
+++ src/utilities/getThemeColorForRoute.js
@@ -0,0 +1,21 @@
+import {
+  portfolioThemColor,
+  skillsThemeColor,
+  connectThemeColor,
+  aboutThemeColor
+} from "../constants/colors";
+
+export default function getThemeColorForRoute(pathname) {
+  switch (pathname) {
+    case "/connect":
+      return connectThemeColor;
+    case "/about":
+      return aboutThemeColor;
+    case "/skills":
+      return skillsThemeColor;
+    case "/portfolio":
+      return portfolioThemColor;
+    default:
+      return connectThemeColor;
+  }
+}
